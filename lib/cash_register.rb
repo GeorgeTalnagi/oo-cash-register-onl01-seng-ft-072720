ca3d1eeb80d451e70ee748df4b946f7a1099ed90
George Talnagi <gtalnagi@yahoo.com>
2020-08-10 00:15:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,8 @@ attr_accessor :total, :discount, :items
   
   def add_item(title, price, quantity = 1)
     i = 0 
-    while i < quantity do 
+    while i < quantity do
+    i += 1 
     end 
     @total += price 
     @items << title 
