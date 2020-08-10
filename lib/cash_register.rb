d95e0ac6ebde41c577236b6039baa5025962a131
George Talnagi <gtalnagi@yahoo.com>
2020-08-10 00:32:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,16 +19,13 @@ attr_accessor :total, :discount, :items
   end 
   
   def apply_discount
-    if @discount == 0 
-      puts "There is no discount to apply."
-    else  
+    if @discount > 0 
       @discount = @discount.to_f 
       @total = @total - (@total * @discount / 100)
       puts "After the discount, the total comes to #{@total}."
+    else  
+      puts "There is no discount to apply."
     end 
-  end 
-  
-  def items 
   end 
   
   def void_last_transaction
